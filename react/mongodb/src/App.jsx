c5142e5ae0062bd6bb69491c74af54052eaf7d15
JohnPaulWeb <braganzajohnpaul@gmail.com>
2026-03-25 20:58:20 +0800

    Learning | React

--- react/mongodb/src/App.jsx
+++ react/mongodb/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import heroImg from './assets/pic.jpg'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function App(){ 
+    X = 'Ayanokoji'; 
+  }
+
   return (
-    <h1>Hello Im a John Welcome to MongoDB</h1>
+
+    <>
+    <PokemonCard />
+     <h1>Hello Im a John Welcome to MongoDB</h1>
+     <img src="https://i.pinimg.com/474x/99/ae/83/99ae8324c99a3101e670383bd2d7d517.jpg" alt="Hero Image" />
+
+  </>
+   
   )
 }
 
---
+++ react/mongodb/src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+
+
+
+// console.log(props);
+function PokemonCard(props) {
+    return(
+        <>
+        {/* <h1>This is the Cat</h1>
+        <img src={heroImg} alt="" /> */}
+      
+        </>
+    )
+}
+
+export default PokemonCard;
